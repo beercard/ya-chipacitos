39994334ba890b84b264c23e058d99aa17563ebe
beercard <yachipacitos@gmail.com>
2026-02-09 19:23:55 -0300

    Update next.config.ts: Add yachipacitos.com.ar to remotePatterns and ignore eslint build errors

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,16 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'logotipoz.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'yachipacitos.com.ar',
+      },
     ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
